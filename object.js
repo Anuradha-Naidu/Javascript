7d3e32c073a2e5ca6ab2ca1e3c105058d50ebb6f
Anuradha Naidu <88324015+Anuradha-Naidu@users.noreply.github.com>
2024-04-14 19:52:18 +0000

    object add

---
+++ object.js
@@ -0,0 +1,19 @@
+//object constructor : singleton
+
+object.create 
+
+// object literals
+//const JsUser = {};
+// can add keys and thier values in object
+
+const JsUser = {
+
+    name: " ",
+    age: " ",
+    location: " ",
+    email: " ", 
+    isLoggedIn :   
+    lasLoginDays : []
+
+
+}
