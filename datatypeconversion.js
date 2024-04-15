06fbf61e0fc3852efa93010480dffe9b4c1278a5
Anuradha Naidu <88324015+Anuradha-Naidu@users.noreply.github.com>
2024-04-15 08:26:27 +0000

    datatype

---
+++ datatypeconversion.js
@@ -0,0 +1,44 @@
+
+//number
+const score = 90
+
+console.log(score);
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+//string
+
+const score1 = "90abc"
+
+console.log(score1);
+console.log(typeof score1);
+
+let valueInNumber2 = Number(score1)
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+
+//null
+const score2 = null
+
+console.log(score2);
+console.log(typeof score2); //null is object datatype
+
+let valueInNumber3 = Number(score2)
+console.log(typeof valueInNumber3);
+console.log(valueInNumber3); // null is 0
+
+//undefined
+const score3 = undefined
+
+console.log(score3);
+console.log(typeof score3); //undefined is undefined datatype only
+
+let valueInNumber4 = Number(score3)
+console.log(typeof valueInNumber4);
+console.log(valueInNumber4); // undefined is NaN
+
+
+
+
